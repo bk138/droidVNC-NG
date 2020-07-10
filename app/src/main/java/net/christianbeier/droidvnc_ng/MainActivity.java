8c4c3057011939919297b1384568211ed8458426
Christian Beier <info@christianbeier.net>
2020-07-10 15:07:13 +0200

    Initial commit: add app scaffold

---
+++ app/src/main/java/net/christianbeier/droidvnc_ng/MainActivity.java
@@ -0,0 +1,14 @@
+package net.christianbeier.droidvnc_ng;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
