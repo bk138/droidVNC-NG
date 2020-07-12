21fc12c98bbef25e88d768778bbf6003e8613473
Christian Beier <info@christianbeier.net>
2020-07-12 13:10:29 +0200

    MainActivity: add PoC screen capturing to file
    Capturing inspired by https://github.com/android/media-samples/tree/master/ScreenCapture
    
    The bits for saving to file are from https://github.com/oduwa/AndroidMirroring/tree/master/Android%20mirroring%20app

--- app/src/main/java/net/christianbeier/droidvnc_ng/MainActivity.java
+++ app/src/main/java/net/christianbeier/droidvnc_ng/MainActivity.java
@@ -23,13 +23,212 @@ package net.christianbeier.droidvnc_ng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_MEDIA_PROJECTION = 42;
+
+    private Button mButtonToggle;
+
+    private int mResultCode;
+    private Intent mResultData;
+
+    public static Bitmap mBmp;
+
+    private ImageReader mImageReader;
+    private int mWidth;
+    private int mHeight;
+    private VirtualDisplay mVirtualDisplay;
+    private MediaProjection mMediaProjection;
+    private MediaProjectionManager mMediaProjectionManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMediaProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+
+        mButtonToggle = (Button) findViewById(R.id.toggle);
+        mButtonToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mVirtualDisplay == null) {
+                    startScreenCapture();
+                } else {
+                    stopScreenCapture();
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_MEDIA_PROJECTION) {
+            if (resultCode != Activity.RESULT_OK) {
+                Log.i(TAG, "User cancelled");
+                Toast.makeText(this, R.string.user_cancelled, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Log.i(TAG, "Starting screen capture");
+            mResultCode = resultCode;
+            mResultData = data;
+            setUpMediaProjection();
+            setUpVirtualDisplay();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopScreenCapture();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        tearDownMediaProjection();
+    }
+
+    private void setUpMediaProjection() {
+        mMediaProjection = mMediaProjectionManager.getMediaProjection(mResultCode, mResultData);
+    }
+
+    private void tearDownMediaProjection() {
+        if (mMediaProjection != null) {
+            mMediaProjection.stop();
+            mMediaProjection = null;
+        }
+    }
+
+    private void startScreenCapture() {
+
+        if (mMediaProjection != null) {
+            setUpVirtualDisplay();
+        } else if (mResultCode != 0 && mResultData != null) {
+            setUpMediaProjection();
+            setUpVirtualDisplay();
+        } else {
+            Log.i(TAG, "Requesting confirmation");
+            // This initiates a prompt dialog for the user to confirm screen projection.
+            startActivityForResult(
+                    mMediaProjectionManager.createScreenCaptureIntent(),
+                    REQUEST_MEDIA_PROJECTION);
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    private void setUpVirtualDisplay() {
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        getWindowManager().getDefaultDisplay().getSize(size);
+
+        mWidth = size.x;
+        mHeight = size.y;
+
+        mImageReader = ImageReader.newInstance(mWidth, mHeight, PixelFormat.RGBA_8888, 2);
+        mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader imageReader) {
+                Log.d(TAG, "image available");
+                Image image = imageReader.acquireLatestImage();
+
+                final Image.Plane[] planes = image.getPlanes();
+                final ByteBuffer buffer = planes[0].getBuffer();
+                int pixelStride = planes[0].getPixelStride();
+                int rowStride = planes[0].getRowStride();
+                int rowPadding = rowStride - pixelStride * mWidth;
+                int w = mWidth + rowPadding / pixelStride;
+
+
+                // Create Bitmap
+                if (mBmp == null) {
+                    mBmp = Bitmap.createBitmap(w, mHeight, Bitmap.Config.ARGB_8888);
+                }
+                buffer.rewind();
+                mBmp.copyPixelsFromBuffer(buffer);
+
+                saveBitmapToFile(mBmp, "vncbitmap.png", getFilesDir().getAbsolutePath(), false);
+
+                image.close();
+            }
+        }, null);
+
+        mVirtualDisplay = mMediaProjection.createVirtualDisplay(getString(R.string.app_name),
+                mWidth, mHeight, metrics.densityDpi,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mImageReader.getSurface(), null, null);
+
+        mButtonToggle.setText(R.string.stop);
+    }
+
+    private void stopScreenCapture() {
+        if (mVirtualDisplay == null) {
+            return;
+        }
+        mVirtualDisplay.release();
+        mVirtualDisplay = null;
+
+        tearDownMediaProjection();
+
+        mButtonToggle.setText(R.string.start);
+    }
+
+
+    public static File saveBitmapToFile(Bitmap bitmap, String filename, String path, boolean recycle) {
+        FileOutputStream out = null;
+        try {
+            File f = new File(path, filename);
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+            out = new FileOutputStream(f);
+            if (bitmap.compress(Bitmap.CompressFormat.PNG, 90, out)) {
+                return f;
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Could not save bitmap", e);
+        } finally {
+            try {
+                out.close();
+            } catch (Throwable ignore) {
+            }
+            if (recycle) {
+                bitmap.recycle();
+            }
+        }
+        return null;
+    }
+
 }
