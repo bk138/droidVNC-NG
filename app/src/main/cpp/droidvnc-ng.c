159efcb135f52b5e22a2a84b0258e9b6e184a9b7
Christian Beier <info@christianbeier.net>
2020-07-13 20:53:26 +0200

    native: wire up logging and add a very simple server

--- app/src/main/cpp/droidvnc-ng.c
+++ app/src/main/cpp/droidvnc-ng.c
@@ -25,16 +25,52 @@
 
 #define TAG "droidvnc-ng (native)"
 
+/*
+ * Modeled after rfbDefaultLog:
+ *  - with Android log functions
+ *  - without time stamping as the Android logging does this already
+ * There's no per-connection log since we cannot find out which client
+ * called the logger function :-(
+ */
+static void logcat_logger(const char *format, ...)
+{
+    va_list args;
+
+    va_start(args, format);
+    __android_log_vprint(ANDROID_LOG_INFO, TAG, format, args);
+    va_end(args);
+}
+
 
 /*
  * The VM calls JNI_OnLoad when the native library is loaded (for example, through System.loadLibrary).
  * JNI_OnLoad must return the JNI version needed by the native library.
+ *  We use this to wire up LibVNCServer logging to logcat.
  */
 JNIEXPORT jint JNI_OnLoad(JavaVM __unused * vm, void __unused * reserved) {
 
     __android_log_print(ANDROID_LOG_INFO, TAG, "loading, using LibVNCServer %s\n", LIBVNCSERVER_VERSION);
 
+    rfbLog = logcat_logger;
+    rfbErr = logcat_logger;
+
     return JNI_VERSION_1_6;
 }
 
+
+JNIEXPORT jboolean JNICALL Java_net_christianbeier_droidvnc_1ng_MainActivity_vncStartServer(JNIEnv *env, jobject thiz) {
+
+    int argc = 0;
+
+    rfbScreenInfoPtr rfbScreen=rfbGetScreen(&argc, NULL, 400, 300, 8, 3, 4);
+    if(!rfbScreen)
+        return JNI_FALSE;
+
+    rfbScreen->frameBuffer=(char*)malloc(400 * 300 * 4);
+
+    rfbInitServer(rfbScreen);
+    rfbRunEventLoop(rfbScreen, -1, TRUE);
+
+    return JNI_TRUE;
+}
 
