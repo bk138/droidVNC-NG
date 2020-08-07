838d75e65fd8ecac41e3e5ed7a72dc6938fa01ab
Christian Beier <info@christianbeier.net>
2020-08-07 23:03:11 +0200

    native: set desktopName to 'Android'

--- app/src/main/cpp/droidvnc-ng.c
+++ app/src/main/cpp/droidvnc-ng.c
@@ -143,6 +143,7 @@ JNIEXPORT jboolean JNICALL Java_net_christianbeier_droidvnc_1ng_MainService_vncS
 
     theScreen->frameBuffer=(char*)calloc(width * height * 4, 1);
     theScreen->ptrAddEvent = onPointerEvent;
+    theScreen->desktopName = "Android";
 
     rfbRegisterTightVNCFileTransferExtension();
 
