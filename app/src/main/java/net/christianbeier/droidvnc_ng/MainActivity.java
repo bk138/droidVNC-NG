d6a0f7543fa1f34d5076df5c8a111108e5e3a1e9
Christian Beier <info@christianbeier.net>
2020-08-08 09:38:39 +0200

    MainActivity: add port setting

--- app/src/main/java/net/christianbeier/droidvnc_ng/MainActivity.java
+++ app/src/main/java/net/christianbeier/droidvnc_ng/MainActivity.java
@@ -22,13 +22,19 @@
 package net.christianbeier.droidvnc_ng;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -73,6 +79,27 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        EditText port = findViewById(R.id.settings_port);
+        port.setText(String.valueOf(prefs.getInt(Constants.PREFS_KEY_SETTINGS_PORT, 5900)));
+        port.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @SuppressLint("ApplySharedPref")
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                SharedPreferences.Editor ed = prefs.edit();
+                ed.putInt(Constants.PREFS_KEY_SETTINGS_PORT, Integer.parseInt(charSequence.toString()));
+                ed.commit();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
     }
 
     @Override
