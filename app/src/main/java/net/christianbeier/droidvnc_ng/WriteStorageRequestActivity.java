17a4d96a002ba0648b662b7e0f5584a2666a3408
Christian Beier <info@christianbeier.net>
2022-05-16 09:32:27 +0200

    Revert "WriteStorageRequestActivity: request MANAGE_EXTERNAL_STORAGE"
    This reverts commit 835dc1bbbcf68a94c98ca627e84ce8cd4f18f1e5.
    
    re #72

--- app/src/main/java/net/christianbeier/droidvnc_ng/WriteStorageRequestActivity.java
+++ app/src/main/java/net/christianbeier/droidvnc_ng/WriteStorageRequestActivity.java
@@ -26,16 +26,11 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.preference.PreferenceManager;
-import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WriteStorageRequestActivity extends AppCompatActivity {
@@ -44,59 +39,37 @@ public class WriteStorageRequestActivity extends AppCompatActivity {
     private static final int REQUEST_WRITE_STORAGE = 44;
     private static final String PREFS_KEY_PERMISSION_ASKED_BEFORE = "write_storage_permission_asked_before";
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // Android 11 and newer
-            if(!Environment.isExternalStorageManager()) {
-                Log.i(TAG, "Has no permission! Ask!");
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Has no permission! Ask!");
+            final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+            /*
+                As per as per https://stackoverflow.com/a/34612503/361413 shouldShowRequestPermissionRationale()
+                returns false also if user was never asked, so keep track of that with a shared preference. Ouch.
+             */
+            if (!prefs.getBoolean(PREFS_KEY_PERMISSION_ASKED_BEFORE, false) || shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 new AlertDialog.Builder(this)
                         .setCancelable(false)
                         .setTitle(R.string.write_storage_title)
                         .setMessage(R.string.write_storage_msg)
                         .setPositiveButton(R.string.yes, (dialog, which) -> {
-                            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, Uri.parse("package:" + BuildConfig.APPLICATION_ID));
-                            startActivityForResult(intent, REQUEST_WRITE_STORAGE);
+                            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_STORAGE);
+                            SharedPreferences.Editor ed = prefs.edit();
+                            ed.putBoolean(PREFS_KEY_PERMISSION_ASKED_BEFORE, true);
+                            ed.apply();
                         })
                         .setNegativeButton(getString(R.string.no), (dialog, which) -> postResultAndFinish(false))
                         .show();
             } else {
-                Log.i(TAG, "Permission already given!");
-                postResultAndFinish(true);
+                postResultAndFinish(false);
             }
         } else {
-            // Android 10 and older
-            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                Log.i(TAG, "Has no permission! Ask!");
-                final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-                /*
-                   As per as per https://stackoverflow.com/a/34612503/361413 shouldShowRequestPermissionRationale()
-                   returns false also if user was never asked, so keep track of that with a shared preference. Ouch.
-                */
-                if (!prefs.getBoolean(PREFS_KEY_PERMISSION_ASKED_BEFORE, false) || shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    new AlertDialog.Builder(this)
-                            .setCancelable(false)
-                            .setTitle(R.string.write_storage_title)
-                            .setMessage(R.string.write_storage_msg)
-                            .setPositiveButton(R.string.yes, (dialog, which) -> {
-                                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_STORAGE);
-                                SharedPreferences.Editor ed = prefs.edit();
-                                ed.putBoolean(PREFS_KEY_PERMISSION_ASKED_BEFORE, true);
-                                ed.apply();
-                            })
-                            .setNegativeButton(getString(R.string.no), (dialog, which) -> postResultAndFinish(false))
-                            .show();
-                } else {
-                    postResultAndFinish(false);
-                }
-            } else {
-                Log.i(TAG, "Permission already given!");
-                postResultAndFinish(true);
-            }
+            Log.i(TAG, "Permission already given!");
+            postResultAndFinish(true);
         }
     }
 
@@ -106,16 +79,6 @@ public class WriteStorageRequestActivity extends AppCompatActivity {
 
         if (requestCode == REQUEST_WRITE_STORAGE) {
             postResultAndFinish(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED);
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.R)
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == REQUEST_WRITE_STORAGE) {
-            postResultAndFinish(Environment.isExternalStorageManager());
         }
     }
 
