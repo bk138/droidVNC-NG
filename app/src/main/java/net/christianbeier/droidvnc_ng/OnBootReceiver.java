1be15eab3ef013ba7ede5116d7ad2c41730a8eab
Christian Beier <info@christianbeier.net>
2023-05-31 13:48:45 +0200

    OnBootReceiver: use Jetpack PreferenceManager

--- app/src/main/java/net/christianbeier/droidvnc_ng/OnBootReceiver.java
+++ app/src/main/java/net/christianbeier/droidvnc_ng/OnBootReceiver.java
@@ -27,7 +27,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
-import android.preference.PreferenceManager;
+import androidx.preference.PreferenceManager;
 import android.util.Log;
 
 
