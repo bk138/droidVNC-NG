c8033c56228c203e4bd29f59c28d918f28520e5d
Christian Beier <info@christianbeier.net>
2023-05-31 13:46:29 +0200

    MainActivity: use Jetpack PreferenceManager

--- app/src/main/java/net/christianbeier/droidvnc_ng/MainActivity.java
+++ app/src/main/java/net/christianbeier/droidvnc_ng/MainActivity.java
@@ -29,7 +29,7 @@ import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import androidx.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
