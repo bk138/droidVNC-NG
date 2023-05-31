38a78cb4b73ad8b581eb2e9fffbcc7d383c9904c
Christian Beier <info@christianbeier.net>
2023-05-31 13:49:05 +0200

    WriteStorageRequestActivity: use Jetpack PreferenceManager

--- app/src/main/java/net/christianbeier/droidvnc_ng/WriteStorageRequestActivity.java
+++ app/src/main/java/net/christianbeier/droidvnc_ng/WriteStorageRequestActivity.java
@@ -28,7 +28,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import androidx.preference.PreferenceManager;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
