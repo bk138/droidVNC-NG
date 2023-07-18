4eba4a87aa3e6811ca9305fb5119f2bd5fd736d2
Christian Beier <info@christianbeier.net>
2023-07-18 09:35:38 +0200

    Utils: take into account status bar on API level <= 30
    Fixes offset mouse pointer views on these platforms.

--- app/src/main/java/net/christianbeier/droidvnc_ng/Utils.kt
+++ app/src/main/java/net/christianbeier/droidvnc_ng/Utils.kt
@@ -1,5 +1,6 @@
 package net.christianbeier.droidvnc_ng
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Rect
 import android.hardware.display.DisplayManager
@@ -19,10 +20,21 @@ object Utils {
     fun getDisplayInset(context: Context, displayId: Int) : Rect {
         val displayManager = context.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        if (Build.VERSION.SDK_INT >= 29) {
             val cutout = displayManager.getDisplay(displayId).cutout
             if(cutout != null) {
                 return Rect(cutout.safeInsetLeft, cutout.safeInsetTop, cutout.safeInsetRight, cutout.safeInsetBottom)
+            }
+        }
+
+        @SuppressLint("InternalInsetResource")
+        @SuppressLint("DiscouragedApi")
+        if (Build.VERSION.SDK_INT <= 30) {
+            // Android up to and including API level 30 have a status bar offset from the display metrics
+            val resources = context.resources
+            val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+            if (resourceId > 0) {
+                return Rect(0, context.resources.getDimensionPixelSize(resourceId), 0, 0)
             }
         }
 
