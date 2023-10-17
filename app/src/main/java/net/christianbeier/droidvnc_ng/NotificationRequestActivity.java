43bb5566b9ea6c33256f9ea7df7e236f42aba4c3
Christian Beier <info@christianbeier.net>
2023-10-17 16:19:22 +0200

    NotificationRequestActivity: provide acces key for MainService
    re #164

--- app/src/main/java/net/christianbeier/droidvnc_ng/NotificationRequestActivity.java
+++ app/src/main/java/net/christianbeier/droidvnc_ng/NotificationRequestActivity.java
@@ -81,6 +81,7 @@ public class NotificationRequestActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, MainService.class);
         intent.setAction(MainService.ACTION_HANDLE_NOTIFICATION_RESULT);
+        intent.putExtra(MainService.EXTRA_ACCESS_KEY, PreferenceManager.getDefaultSharedPreferences(this).getString(Constants.PREFS_KEY_SETTINGS_ACCESS_KEY, new Defaults(this).getAccessKey()));
         startService(intent);
         finish();
     }
