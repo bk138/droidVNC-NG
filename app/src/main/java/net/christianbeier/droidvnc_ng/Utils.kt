33d6af7446d04fd5f3828efbbd62acf2be20eb38
Christian Beier <info@christianbeier.net>
2024-08-15 16:53:12 +0200

    Utils: add function that copies a directory from assets
    re #6

--- app/src/main/java/net/christianbeier/droidvnc_ng/Utils.kt
+++ app/src/main/java/net/christianbeier/droidvnc_ng/Utils.kt
@@ -6,6 +6,10 @@ import android.os.Build
 import android.provider.Settings
 import android.util.DisplayMetrics
 import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
 import java.io.InputStreamReader
 
 object Utils {
@@ -37,6 +41,48 @@ object Utils {
             } else {
                 ctx.getString(R.string.app_name)
             }
+        }
+    }
+
+    @JvmStatic
+    fun Context.copyAssetsToDir(assetDir: String, outDir: String) {
+        val assetManager = assets
+        val outDirFile = File(outDir)
+
+        try {
+            val files = assetManager.list(assetDir) ?: return
+
+            // Ensure the output directory exists
+            if (!outDirFile.exists()) {
+                outDirFile.mkdirs()
+            }
+
+            for (filename in files) {
+                val assetPath = "$assetDir/$filename"
+                val outFile = File(outDirFile, filename)
+
+                // Check if this is a directory or a file
+                if (assetManager.list(assetPath)?.isNotEmpty() == true) {
+                    // If it's a directory, recursively copy its contents
+                    copyAssetsToDir(assetPath, outFile.absolutePath)
+                } else {
+                    // If it's a file, copy it
+                    val inStream: InputStream = assetManager.open(assetPath)
+                    val outStream = FileOutputStream(outFile)
+
+                    val buffer = ByteArray(1024)
+                    var bytesRead: Int
+                    while (inStream.read(buffer).also { bytesRead = it } != -1) {
+                        outStream.write(buffer, 0, bytesRead)
+                    }
+
+                    inStream.close()
+                    outStream.flush()
+                    outStream.close()
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 
