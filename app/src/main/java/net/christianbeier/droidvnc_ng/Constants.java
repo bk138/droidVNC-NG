eea4729f97a3588a0a4f853dadba73da0e24e19f
Charles Rosaaen <charles@rosaaen.dev>
2026-05-28 15:49:40 -0700

    Add translate numpad constant

--- app/src/main/java/net/christianbeier/droidvnc_ng/Constants.java
+++ app/src/main/java/net/christianbeier/droidvnc_ng/Constants.java
@@ -32,6 +32,7 @@ public class Constants {
     public static final String PREFS_KEY_SETTINGS_SCALING = "settings_scaling" ;
     public static final String PREFS_KEY_SETTINGS_VIEW_ONLY = "settings_view_only" ;
     public static final String PREFS_KEY_SETTINGS_SHOW_POINTERS = "settings_show_pointers" ;
+    public static final String PREFS_KEY_SETTINGS_TRANSLATE_NUMPAD = "settings_translate_numpad" ;
     public static final String PREFS_KEY_SETTINGS_ACCESS_KEY = "settings_access_key";
     public static final String PREFS_KEY_SETTINGS_FILE_TRANSFER = "settings_file_transfer";
 
